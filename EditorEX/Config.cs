998daa8701579063c7b178ee3e0b9325192e9243
Kaitlyn <87990853+ItsKaitlyn03@users.noreply.github.com>
2022-05-17 16:35:48 -0400

    Initial source

---
+++ EditorEX/Config.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using IPA.Config.Stores;
+
+[assembly: InternalsVisibleTo(GeneratedStore.AssemblyVisibilityTarget)]
+namespace EditorEX
+{
+    internal class Config
+    {
+#pragma warning disable CS8632
+        public event Action<Config>? Updated;
+#pragma warning restore CS8632
+
+        public virtual void Changed()
+        {
+            Updated?.Invoke(this);
+        }
+    }
+}
---
+++ EditorEX/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using IPA;
+using IPA.Config.Stores;
+using IPA.Loader;
+using SiraUtil.Attributes;
+using SiraUtil.Zenject;
+using IPAConfig = IPA.Config.Config;
+using IPALogger = IPA.Logging.Logger;
+
+namespace EditorEX
+{
+    [Plugin(RuntimeOptions.DynamicInit), NoEnableDisable, Slog]
+    public class Plugin
+    {
+        [Init]
+        public Plugin(IPALogger logger, IPAConfig conf, PluginMetadata metadata, Zenjector zenjector)
+        {
+            zenjector.UseLogger(logger);
+
+            Config config = conf.Generated<Config>();
+            zenjector.Install(Location.App, container =>
+            {
+                container.BindInstance(config).AsSingle();
+                container.BindInstance(new UBinder<Plugin, PluginMetadata>(metadata));
+            });
+
+            zenjector.Install<EXEditorInstaller>(Location.App); // TODO: Editor
+        }
+    }
+}
