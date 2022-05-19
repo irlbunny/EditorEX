b6d2ecd13d964678140b3a5cfeaf3f12769f7cac
Kaitlyn <87990853+ItsKaitlyn03@users.noreply.github.com>
2022-05-19 09:08:23 -0400

    Fix negative obstacle duration exception

---
+++ EditorEX/HarmonyPatches/ObstaclesRepository.cs
@@ -0,0 +1,23 @@
+﻿using BeatmapEditor3D;
+using BeatmapEditor3D.DataModels;
+using HarmonyLib;
+using IntervalTree;
+using IPA.Utilities;
+using System.Collections.Generic;
+
+namespace EditorEX.HarmonyPatches
+{
+    // Fixes some exceptions with loading obstacles with negative duration.
+    [HarmonyPatch(typeof(ObstaclesRepository), "Add", typeof(ObstacleEditorData))]
+    internal class ObstaclesRepositoryAdd
+    {
+        private static bool Prefix(ObstaclesRepository __instance, ObstacleEditorData obstacle)
+        {
+            var tree = __instance.GetField<IntervalTree<float, ObstacleEditorData>, ObstaclesRepository>("_tree");
+            var dictionary = __instance.GetField<IDictionary<BeatmapEditorObjectId, ObstacleEditorData>, ObstaclesRepository>("_dictionary");
+            tree.Add(obstacle.beat, obstacle.beat + (obstacle.duration < 0 ? -obstacle.duration : obstacle.duration), obstacle);
+            dictionary.Add(obstacle.id, obstacle);
+            return false;
+        }
+    }
+}
--- EditorEX/Plugin.cs
+++ EditorEX/Plugin.cs
@@ -1,16 +1,21 @@
-﻿using IPA;
+﻿using HarmonyLib;
+using IPA;
 using IPA.Config.Stores;
 using IPA.Loader;
 using SiraUtil.Attributes;
 using SiraUtil.Zenject;
+using System.Reflection;
 using IPAConfig = IPA.Config.Config;
 using IPALogger = IPA.Logging.Logger;
 
 namespace EditorEX
 {
-    [Plugin(RuntimeOptions.DynamicInit), NoEnableDisable, Slog]
+    [Plugin(RuntimeOptions.DynamicInit), Slog]
     public class Plugin
     {
+        internal const string HARMONYID = "com.github.ItsKaitlyn03.EditorEX";
+        internal static Harmony HarmonyInstance { get; private set; } = new Harmony(HARMONYID);
+
         [Init]
         public Plugin(IPALogger logger, IPAConfig conf, PluginMetadata metadata, Zenjector zenjector)
         {
@@ -23,7 +28,19 @@ namespace EditorEX
                 container.BindInstance(new UBinder<Plugin, PluginMetadata>(metadata));
             });
 
-            zenjector.Install<EXEditorInstaller>(Location.App); // TODO: Editor
+            //zenjector.Install<EXEditorInstaller>(Location.App); // TODO: Editor
+        }
+
+        [OnEnable]
+        public void OnEnable()
+        {
+            HarmonyInstance.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [OnDisable]
+        public void OnDisable()
+        {
+            HarmonyInstance.UnpatchSelf();
         }
     }
 }
